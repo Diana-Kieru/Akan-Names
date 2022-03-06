e6b1d252cc230740813b4644b312f00046d6cd0b
diana <dianakieru1@gmil.com>
2022-03-06 14:19:29 +0300

    added dates

---
+++ script.js
@@ -0,0 +1,13 @@
+//array
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNmaes = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function checkDay(){
+    let DD =document.getElementById("day").value 
+    let MM =document.getElementById("month").value
+    let date =document.getElementById("year").value
+    let CC =date.substring(0,2)
+    let YY =date.substring(2)
+    let gender =document.calculate.gender.value
+    
+}
