9d5c5b05e32e2d0b4c2e283551aff56a82ced56c
diana <dianakieru1@gmil.com>
2022-03-06 15:19:08 +0300

    stylled the dates

--- script.js
+++ script.js
@@ -9,5 +9,11 @@ function checkDay(){
     let CC =date.substring(0,2)
     let YY =date.substring(2)
     let gender =document.calculate.gender.value
+    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+    
+    if(DD <= 0 || DD > 31){
+        alert("invalid date!")
+    }
+    
     
 }
